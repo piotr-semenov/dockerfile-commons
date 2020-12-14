07f008a0f28daaf8a6809469f8d610ac89833525
Piotr Semenov <piotr.k.semenov@gmail.com>
2020-12-14 20:30:57 +0300

    implement lint-dockerfiles target

---
+++ lint-dockerfiles.mk
@@ -0,0 +1,13 @@
+HADOLINT_CONFIG ?= `pwd`/dockerfile-commons/.hadolint.yaml
+
+
+.PHONY: lint-dockerfiles
+lint-dockerfiles: export FILES = $(shell find . -type f -name "Dockerfile.*")
+lint-dockerfiles:  ## Lints the Dockerfile.*.
+	@for p in $$FILES; do \
+	  docker run --rm \
+	             -v $(HADOLINT_CONFIG):/tmp/.hadolint.yaml:ro \
+	             -v `pwd`:/workdir \
+	             -w /workdir \
+	             -i hadolint/hadolint /bin/hadolint -f tty -c /tmp/.hadolint.yaml $$p; \
+	done
