857696cd43a2701c6278671af5232bb717c5ce3d
Piotr Semenov <piotr.k.semenov@gmail.com>
2020-12-16 22:36:11 +0300

    fix, make build_docker_image and goss_docker_image to accept only strings as inputs arguments

--- docker-funcs.mk
+++ docker-funcs.mk
@@ -1,8 +1,8 @@
 # Customize the build process of docker image.
 # Args:
-#     $(1): The target image tag.
-#     $(2): The whitespace-separated list of build-arg=value pairs.
-#     $(3), optional: The additional flags and args for `docker build`. By default, it is "-f Dockerfile `pwd`".
+#     $(1) (str): The target image tag.
+#     $(2) (str): The whitespace-separated list of build-arg=value pairs.
+#     $(3) (str, optional): The additional flags and args for `docker build`. By default, it is "-f Dockerfile `pwd`".
 # Examples:
 #     $(call build_docker_image,"test","vcsref=$$(git rev-parse --short HEAD)","-f Dockerfile.test .")
 define build_docker_image
@@ -16,9 +16,9 @@ endef
 
 # Customize the goss-based testing of docker image.
 # Args:
-#     $(1): The docker image tag under test.
-#     $(2), optional: The path to the target test yaml. By default, it is "`pwd`/tests/test.yaml".
-#     $(3), optional: The whitespace-separated list of var=value environment passed to goss.
+#     $(1) (str): The docker image tag under test.
+#     $(2) (str, optional): The path to the target test yaml. By default, it is "`pwd`/tests/test.yaml".
+#     $(3) (str, optional): The whitespace-separated list of var=value environment passed to goss.
 # Examples:
 #     $(call goss_docker_image,test,tests/main.yaml)
 define goss_docker_image
@@ -26,6 +26,6 @@ define goss_docker_image
 	GOSS_FILE=$(if $(2),$(shell basename $(2)),test.yaml)\
 	GOSS_FILES_STRATEGY=cp\
 	$(shell which dgoss) run --entrypoint=/bin/sh \
-	                         $(shell echo $(3) | xargs -n1 -I@ echo "--env @") \
+	                         $(shell echo $(3) | tr ' ' '\n' | xargs -n1 -I@ echo "--env @") \
 	                         -it $(1)
 endef
