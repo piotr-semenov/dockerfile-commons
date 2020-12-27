5dbbdab9fb7ce782eecc2355637f1605e0a233df
Piotr Semenov <piotr.k.semenov@gmail.com>
2020-12-27 16:42:19 +0300

    implement scan-docker target

---
+++ scan-docker.mk
@@ -0,0 +1,34 @@
+IMAGE_NAME ?=
+
+define _check_service_are_healthy
+	export containers=$$(docker-compose -f $(1) ps -q $(2)) &&\
+	export n_containers=$$(echo "$$containers" | wc -w) &&\
+	export states=$$(echo "$$containers" | xargs -I@ docker inspect -f '{{if .State.Running}}{{ .State.Health.Status }}{{end}}' @) &&\
+	if [[ $$(echo "$$states" | grep 'healthy' | wc -l) = "$$n_containers" ]]; \
+	 then echo "healthy"; \
+	else \
+	 echo "unhealthy"; \
+	fi
+endef
+
+define _wait_healthy_containers
+	until [[ $$(eval 'echo "$(call _check_service_are_healthy,$(1),$(2))"') != "healthy" ]]; \
+	 do echo "Services $(2): waiting for status 'healthy'..." && sleep 1; \
+	done;
+endef
+
+
+scan_anchore: export DOCKER_COMPOSE_FILE:=$(shell mktemp docker-compose.yaml.XXXXXX)
+scan-docker:  ## Scans the \$IMAGE_NAME for vulnerabilities via Anchore.
+	@curl -q https://engine.anchore.io/docs/quickstart/docker-compose.yaml 2> /dev/null 1> $(DOCKER_COMPOSE_FILE)
+	@docker-compose -f $(DOCKER_COMPOSE_FILE) up -d
+	@$(call _wait_healthy_containers,$(DOCKER_COMPOSE_FILE),)
+	@docker run --net=host\
+	            -e ANCHORE_CLI_URL=http://localhost:8228/v1/\
+	            -i anchore/engine-cli\
+	            /bin/bash -c "anchore-cli image add $(IMAGE_NAME) 1> /dev/null &&\
+	                          anchore-cli image wait $(IMAGE_NAME) 1> /dev/null &&\
+	                          anchore-cli image vuln $(IMAGE_NAME) all &&\
+	                          anchore-cli evaluate check $(IMAGE_NAME)"
+	@docker-compose -f $(DOCKER_COMPOSE_FILE) down
+	@rm -f $(DOCKER_COMPOSE_FILE)
