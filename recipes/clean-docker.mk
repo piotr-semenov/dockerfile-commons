287397e1ef5fa404fea44198fc1c123064f7f250
Piotr Semenov <piotr.k.semenov@gmail.com>
2024-09-20 02:19:32 +0400

    fix: update clean-docker recipe not to fail if there is no things to clean out

--- recipes/clean-docker.mk
+++ recipes/clean-docker.mk
@@ -1,5 +1,10 @@
 .PHONY: clean-docker
 clean-docker:  ## Removes the intermediate & dangling docker images + already exited containers.
-	-@docker rm -v $(shell docker ps -q --filter status=exited) 2> /dev/null
-	-@docker rmi $(shell docker images -q --filter "label=stage=intermediate") 2> /dev/null
-	-@docker rmi $(shell docker images -q --filter "dangling=true") 2> /dev/null
+	$(eval _CONTAINERS_LIST := $(shell docker ps -q --filter status=exited))
+	@$(if $(strip $(_CONTAINERS_LIST)), \
+	 docker rm -v $(_CONTAINERS_LIST) 2> /dev/null, \
+	)
+	@for p in "label=stage=intermediate" "dangling=true"; do \
+	 _IMAGES_LIST=$$(docker images -q --filter "$$p"); \
+	 if [[ -n "$${_IMAGES_LIST}" ]]; then docker rmi $${_IMAGES_LIST}; fi; \
+	done
